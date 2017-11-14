0c57c8067134903e9145bc2910fb8b596564113d
Dumitras <alphonzoalph@gmail.com>
2017-11-14 17:17:18 +0200

    Fixes

--- core/store.js
+++ core/store.js
@@ -1,7 +1,7 @@
 const { applyMiddleware, createStore } = require('redux')
 const ReduxThunk = require('redux-thunk')
 const Promise = require('redux-promise-middleware')
-const middleware = applyMiddleware(Promise.default(), ReduxThunk)
+const middleware = applyMiddleware(Promise.default(), ReduxThunk.default)
 
 module.exports = (config) => {
     return createStore(config.reducer, config.initialState, middleware);
